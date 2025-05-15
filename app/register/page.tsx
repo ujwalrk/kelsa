3f81bc20f81041a618a1176c7e81b2db8ffa03a5
ujwalrk <ujwa;.png@gmail.com>
2025-05-15 19:46:38 +0530

    initial1

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 import { useState } from 'react'
-import { supabase } from '@/lib/supabase'
+import { supabase } from '../../lib/supabase'
 import { useRouter } from 'next/navigation'
 import { Box, TextField, Button, Typography } from '@mui/material'
 
